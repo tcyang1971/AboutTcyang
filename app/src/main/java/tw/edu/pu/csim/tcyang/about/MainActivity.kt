79ca35f6329e4bc11a7cd5dfbaaed2b78e59d319
tcyang <tcyang@gm.pu.edu.tw>
2023-03-22 13:51:58 +0800

    v.1(楊子青)

--- app/src/main/java/tw/edu/pu/csim/tcyang/about/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/about/MainActivity.kt
@@ -2,10 +2,25 @@ package tw.edu.pu.csim.tcyang.about
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txv:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv = findViewById(R.id.txv)
+        txv.setOnClickListener({
+            if (txv.text == "子青您好!"){
+                txv.text = "海青班很棒"
+            }
+            else{
+                txv.text = "子青您好!"
+            }
+        })
     }
+
+
 }
